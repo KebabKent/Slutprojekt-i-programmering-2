dabd8acfa6f7969e29e903448724301ab6fa82ef
Niko O <nikodemus.ohm2@gmail.com>
2022-03-28 09:21:04 +0200

    fixa små saker

--- slutprojekt.py
+++ slutprojekt.py
@@ -1,43 +1,83 @@
 import requests
 
-x = requests.get('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5')
-gg = x.json()
-print(gg)
 
 
+""" Klasser för anställda """
 class Employee:
-    def __init__(self, name, proffesion):
+    def __init__(self, name, password):
         self.name = name
+        self.password = password
+
+class Admin(Employee):
+    def __init__(self, name, password, proffesion):
+        super().__init__(name, password)
+        self.proffesion = proffesion
+
+class User(Employee):
+    def __init__(self, name, password, proffesion):
+        super().__init__(name, password)
         self.proffesion = proffesion
 
 
-class Admin:
-    def __init__(self, name, proffesion, password):
-        super().__init__(name, proffesion)
-        self.password = password
 
-
+""" Klasser för att sortera djuren """
 class Animal:
-    def __init__(self, name):
-        self.name = name
-
-
-class Fish(Animal):
     def __init__(self, name, family, genus):
-        super().__init__(name)
+        self.name = name
         self.family = family
         self.genus = genus
 
+class Fish(Animal):
+    def __init__(self, name, family, genus, count):
+        super().__init__(name, family, genus)
+        self.count = count
+        
 class Amphibian(Animal):
-    pass
+   def __init__(self, name, family, genus, count):
+        super().__init__(name, family, genus)
+        self.count = count
 
 class Reptile(Animal):
-    pass
+   def __init__(self, name, family, genus, count):
+        super().__init__(name, family, genus)
+        self.count = count
 
 class Bird(Animal):
-    pass
+    def __init__(self, name, family, genus, count):
+        super().__init__(name, family, genus)
+        self.count = count
 
 class Mammal(Animal):
-    pass
+   def __init__(self, name, family, genus, count):
+        super().__init__(name, family, genus)
+        self.count = count
+print('din mamma')
+
+
+
+def program_loop():
+    print('din mamma ligger på pizza')
+
+
+
+def get_num():
+    x = requests.get('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5')
+    gg = x.json()
+    print(gg)
+
+
+
+if __name__ == '__main__':
+    """ 
+    Här ska de anställda hämtas in från excell filen med hjälp av 
+    pythons excell bibliotek. En klass användare per anställd.
+    """
+    program_loop()
+    get_num()
+
+
+
+
+
 
 
