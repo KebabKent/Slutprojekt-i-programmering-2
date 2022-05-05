9fe19c46d2023df88af4955d5e857cd95d983767
Niko O <nikodemus.ohm2@gmail.com>
2022-05-05 15:08:50 +0200

    fixade inloggnings funktionen

--- slutprojekt.py
+++ slutprojekt.py
@@ -6,29 +6,47 @@ from getpwd import getpwd
 
 """ Klasser för anställda """
 class Bank:
-    def __init__(self, name, password, proffession):
+    def __init__(self, name, password, proffession, row):
         self.name = name
         self.__password = password
         self.proffession = proffession
+        self.row = row
         """ Här ska alla metoder som båda klasserna har gemensamma vara """
     
     def return_password(self):
         return self.__password
-
+    
+    def return_row(self):
+        return self.row
 
 class Admin(Bank):
-    def __init__(self, name, password, proffession):
-        super().__init__(name, password, proffession)
+    def __init__(self, name, password, proffession, row):
+        super().__init__(name, password, proffession, row)
         # Ärv metoder som admin ska komma åt
+    
+    def shange_password(self):
+        pass
+
+    def shange_user_password():
+        pass
 
 class User(Bank):
-    def __init__(self, name, password, proffession):
-        super().__init__(name, password, proffession)
+    def __init__(self, name, password, proffession, row):
+        super().__init__(name, password, proffession, row)
         # Ärv metoder som user ska komma åt
+    
+    def shange_password(self):
+        pass
+
+    def check_account_balance(self):
+        pass
 
 class Underage_user(Bank):
-    def __init__(self,name, password, proffession):
-        super().__init__(name, password, proffession)
+    def __init__(self,name, password, proffession, row):
+        super().__init__(name, password, proffession, row)
+
+    def check_account_balance(self):
+        pass
 
 
 
@@ -38,42 +56,61 @@ def login():
     sheet_obj = wb_obj.active
     max_ro = sheet_obj.max_row
 
-    anv_nam = str(input('Username: '))
 
-    for x in range(1, max_ro + 1):
-        anv = sheet_obj.cell(row = x, column = 1)
 
-        if anv.value == anv_nam:
-            password = getpwd() #Frågar om lösenord och gör så att man inte ser vad som skrivs in
-            print(password) # Göra så att man kan välja att se lösenordet innan man skriver in det
-            pass_check = sheet_obj.cell(row = x, column = 3)
+    """ Kollar om användaren finns och vart den finns"""
+    for i in range(3):
+        anv_nam = str(input('Username: '))
 
-            if password == pass_check.value:
-                print('Du hade rätt lösenord')
-
-                """ Gör ett klass objekt av användaren """
-
-                role = sheet_obj.cell(row = x, column = 5)
-                profession = sheet_obj.cell(row = x, column = 6)
-                
-                if role.value == 'User':
-                    print('User')
-                    logged_in_user = User(anv_nam, pass_check.value, profession.value)
-                    print(logged_in_user.proffession, logged_in_user.return_password())
-                    return logged_in_user
-                
-                elif role.value == 'Underage_user':
-                    print('Underage_user')
-                    logged_in_user = Underage_user(anv_nam, pass_check.value, profession.value)
-                    print(logged_in_user.proffession, logged_in_user.return_password())
-                    return logged_in_user
-
-                elif role.value == 'Admin':
-                    print('Admin')
-                    logged_in_user = Admin(anv_nam, pass_check.value, profession.value)
-                    return logged_in_user
-
+        for xx in range(1, max_ro +1):
+            anv = sheet_obj.cell(row = xx, column = 1)
+            if anv.value == anv_nam:
+                print('Användaren finns')
                 break
+        
+        if anv.value == anv_nam:
+            break
+        else:
+            print('Användaren finns inte försök igen')
+
+
+
+    """ Tar lösenordet och loggar in användaren med användarnamnet """
+    if anv.value == anv_nam:
+        for ii in range(3):
+            anv = sheet_obj.cell(row = xx, column = 1)
+
+            if anv.value == anv_nam:
+                password = getpwd() #Frågar om lösenord och gör så att man inte ser vad som skrivs in
+                print(password) # Göra så att man kan välja att se lösenordet innan man skriver in det
+                pass_check = sheet_obj.cell(row = xx, column = 3)
+
+                if password == pass_check.value:
+                    print('Du hade rätt lösenord')
+
+                    """ Gör ett klass objekt av användaren """
+
+                    role = sheet_obj.cell(row = xx, column = 5)
+                    profession = sheet_obj.cell(row = xx, column = 6)
+                    row = xx
+                    
+                    if role.value == 'User':
+                        print('User')
+                        logged_in_user = User(anv_nam, pass_check.value, profession.value, row)
+                        print(logged_in_user.proffession, logged_in_user.return_password())
+                        return logged_in_user
+                    
+                    elif role.value == 'Underage_user':
+                        print('Underage_user')
+                        logged_in_user = Underage_user(anv_nam, pass_check.value, profession.value, row)
+                        print(logged_in_user.proffession, logged_in_user.return_password())
+                        return logged_in_user
+
+                    elif role.value == 'Admin':
+                        print('Admin')
+                        logged_in_user = Admin(anv_nam, pass_check.value, profession.value, row)
+                        return logged_in_user
+                    break
 
 
 
@@ -94,12 +131,10 @@ if __name__ == '__main__':
     Här ska de anställda hämtas in från excell filen med hjälp av 
     pythons excell bibliotek. En klass användare per anställd.
     """
-    login()
+    user = login()
     program_loop()
-    get_num()
-
-
-
+    #get_num()
+    print('Ligger på rad: ', user.return_row())
 
 
 
